f060f67832bd3eee206caf6581afb7da74e462bf
Mateus <mattoss.barreto@gmail.com>
2025-02-17 15:05:58 -0300

    switched api ip address

--- src/utils/baseUrls.ts
+++ src/utils/baseUrls.ts
@@ -1,2 +1,2 @@
 export const imgBucketBaseUrl = "https://furniro-images.s3.us-east-2.amazonaws.com";
-export const dbApiBaseUrl = "http://3.145.17.162:3000"
+export const dbApiBaseUrl = "http://18.225.156.156:3000/"
